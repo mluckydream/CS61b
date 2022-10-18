109850a34e21577917433a8d46a7010bbba65cb9
mluckydream <2274607609@qq.com>
2022-10-18 09:18:20 +0800

    feat: complete lab9

--- lab9/lab9tester/TestBSTMap.java
+++ lab9/lab9tester/TestBSTMap.java
@@ -1,22 +1,22 @@
 package lab9tester;
 
-import static org.junit.Assert.*;
-
+import lab9.MyHashMap;
 import org.junit.Test;
-import lab9.BSTMap;
+
+import static org.junit.Assert.*;
 
 /**
  * Tests by Brendan Hu, Spring 2015, revised for 2018 by Josh Hug
  */
-public class TestBSTMap {
+public class TestMyHashMap {
 
     @Test
     public void sanityGenericsTest() {
         try {
-            BSTMap<String, String> a = new BSTMap<String, String>();
-            BSTMap<String, Integer> b = new BSTMap<String, Integer>();
-            BSTMap<Integer, String> c = new BSTMap<Integer, String>();
-            BSTMap<Boolean, Integer> e = new BSTMap<Boolean, Integer>();
+            MyHashMap<String, String> a = new MyHashMap<String, String>();
+            MyHashMap<String, Integer> b = new MyHashMap<String, Integer>();
+            MyHashMap<Integer, String> c = new MyHashMap<Integer, String>();
+            MyHashMap<Boolean, Integer> e = new MyHashMap<Boolean, Integer>();
         } catch (Exception e) {
             fail();
         }
@@ -25,15 +25,13 @@ public class TestBSTMap {
     //assumes put/size/containsKey/get work
     @Test
     public void sanityClearTest() {
-        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        MyHashMap<String, Integer> b = new MyHashMap<String, Integer>();
         for (int i = 0; i < 455; i++) {
-            b.put("hi" + i, 1 + i);
+            b.put("hi" + i, 1);
             //make sure put is working via containsKey and get
-            assertTrue(null != b.get("hi" + i));
-            assertTrue(b.get("hi" + i).equals(1 + i));
-            assertTrue(b.containsKey("hi" + i));
+            assertTrue(null != b.get("hi" + i)
+                    && b.containsKey("hi" + i));
         }
-        assertEquals(455, b.size());
         b.clear();
         assertEquals(0, b.size());
         for (int i = 0; i < 455; i++) {
@@ -44,7 +42,7 @@ public class TestBSTMap {
     // assumes put works
     @Test
     public void sanityContainsKeyTest() {
-        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        MyHashMap<String, Integer> b = new MyHashMap<String, Integer>();
         assertFalse(b.containsKey("waterYouDoingHere"));
         b.put("waterYouDoingHere", 0);
         assertTrue(b.containsKey("waterYouDoingHere"));
@@ -53,21 +51,19 @@ public class TestBSTMap {
     // assumes put works
     @Test
     public void sanityGetTest() {
-        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        MyHashMap<String, Integer> b = new MyHashMap<String, Integer>();
         assertEquals(null, b.get("starChild"));
-        assertEquals(0, b.size());
         b.put("starChild", 5);
-        assertTrue(((Integer) b.get("starChild")).equals(5));
-        b.put("KISS", 5);
-        assertTrue(((Integer) b.get("KISS")).equals(5));
         assertNotEquals(null, b.get("starChild"));
-        assertEquals(2, b.size());
+        b.put("KISS", 5);
+        assertNotEquals(null, b.get("KISS"));
+        assertNotEquals(null, b.get("starChild"));
     }
 
     // assumes put works
     @Test
     public void sanitySizeTest() {
-        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        MyHashMap<String, Integer> b = new MyHashMap<String, Integer>();
         assertEquals(0, b.size());
         b.put("hi", 1);
         assertEquals(1, b.size());
@@ -80,13 +76,58 @@ public class TestBSTMap {
     //assumes get/containskey work
     @Test
     public void sanityPutTest() {
-        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        MyHashMap<String, Integer> b = new MyHashMap<String, Integer>();
         b.put("hi", 1);
-        assertTrue(b.containsKey("hi"));
-        assertTrue(b.get("hi") != null);
+        assertTrue(b.containsKey("hi") && b.get("hi") != null);
+    }
+
+    /*
+     * Test for general functionality and that the properties of Maps hold.
+     */
+    @Test
+    public void functionalityTest() {
+        MyHashMap<String, String> dictionary = new MyHashMap<>();
+        assertEquals(0, dictionary.size());
+
+        // can put objects in dictionary and get them
+        dictionary.put("hello", "world");
+        assertTrue(dictionary.containsKey("hello"));
+        assertEquals("world", dictionary.get("hello"));
+        assertEquals(1, dictionary.size());
+
+        // putting with existing key updates the value
+        dictionary.put("hello", "kevin");
+        assertEquals(1, dictionary.size());
+        assertEquals("kevin", dictionary.get("hello"));
+
+        // putting key in multiple times does not affect behavior
+        MyHashMap<String, Integer> studentIDs = new MyHashMap<>();
+        studentIDs.put("sarah", 12345);
+        assertEquals(1, studentIDs.size());
+        assertEquals(12345, studentIDs.get("sarah").intValue());
+        studentIDs.put("alan", 345);
+        assertEquals(2, studentIDs.size());
+        assertEquals(12345, studentIDs.get("sarah").intValue());
+        assertEquals(345, studentIDs.get("alan").intValue());
+        studentIDs.put("alan", 345);
+        assertEquals(2, studentIDs.size());
+        assertEquals(12345, studentIDs.get("sarah").intValue());
+        assertEquals(345, studentIDs.get("alan").intValue());
+        studentIDs.put("alan", 345);
+        assertEquals(2, studentIDs.size());
+        assertEquals(12345, studentIDs.get("sarah").intValue());
+        assertEquals(345, studentIDs.get("alan").intValue());
+        assertTrue(studentIDs.containsKey("sarah"));
+        assertTrue(studentIDs.containsKey("alan"));
+
+        // handle values being the same
+        assertEquals(345, studentIDs.get("alan").intValue());
+        studentIDs.put("evil alan", 345);
+        assertEquals(345, studentIDs.get("evil alan").intValue());
+        assertEquals(studentIDs.get("evil alan"), studentIDs.get("alan"));
     }
 
     public static void main(String[] args) {
-        jh61b.junit.TestRunner.runTests(TestBSTMap.class);
+        jh61b.junit.TestRunner.runTests(TestMyHashMap.class);
     }
 }
